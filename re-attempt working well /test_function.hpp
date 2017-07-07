df996c4d8b5b089da6f5938b382eb20e3b84c50b
kartik nighania <kkstrack@gmail.com>
2017-07-08 03:48:56 +0530

    single test function file made

--- re-attempt working well /test_function.hpp
+++ re-attempt working well /test_function.hpp
@@ -1,6 +1,6 @@
 /**
  * @file test_function.hpp
- * @author Ryan Curtin
+ * @Kartik Nighania (Mentor Marcus Edel)
  *
  * Very simple test function for CMAES (the same test used in sgd optimiser)
  *
@@ -12,16 +12,13 @@
 #ifndef MLPACK_CORE_OPTIMIZERS_CMAES_TEST_FUNCTION_HPP
 #define MLPACK_CORE_OPTIMIZERS_CMAES_TEST_FUNCTION_HPP
 
-#include <mlpack/core.hpp>
-#include <armadillo>
-#include <iostream>
+#include <mlpack/prereqs.hpp>
 
 namespace mlpack {
 namespace optimization {
 namespace test {
 
-//! Very, very simple test function which is the composite of three other
-//! functions.  Its a derivative free optimization resulting in no use of gradient
+//! Its a derivative free optimization resulting in no use of gradient
 //! like in stochastic gradient descent and instead uses search points taken randomly
 //! of a normal distribution and moving the mean of the population towards the best 
 //! fitness offsprings.
@@ -30,11 +27,11 @@ class cmaesTestFunction
 
  public:
   //! Return 3 (the number of functions) = the variable N in CMAES class for dimension
- ssize_t NumFunctions(void){ return 3; }
+ ssize_t NumFunctions() const { return 3; }
 
- double Evaluate(arma::mat& coordinates)
+ double Evaluate(const arma::mat& coordinates) const
 	{
-	 	return (-std::exp(-std::abs(coordinates[0])) + 
+	   return (-std::exp(-std::abs(coordinates[0])) + 
 	            std::pow(coordinates[1], 2) + 
 	            std::pow(coordinates[2], 4) + 3 * std::pow(coordinates[2], 2));
 	}
