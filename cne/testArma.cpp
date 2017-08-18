19080cd899824cea9e7e5f0bbf7aa58eb0f928a9
kartik nighania <kkstrack@gmail.com>
2017-08-19 03:56:32 +0530

    working till now

--- cne/testArma.cpp
+++ cne/testArma.cpp
@@ -1,56 +1,46 @@
 #include <iostream>
 #include <mlpack/core.hpp>
+#include <armadillo>
 
 using namespace std;
 using namespace arma;
 
 int main()
-{   mlpack::math::RandomSeed(std::time(NULL));
-    arma::uvec indices = arma::shuffle(arma::linspace<arma::uvec>(0, 10, 11));
-    arma::uvec indice1 = arma::shuffle(arma::linspace<arma::uvec>(0, 10, 11));
+{   
+  arma::cube x(3,3,3);
+  x.fill(4);
 
-
-   arma::mat x(3,3);
-   x.randu();
-
-   cout << endl;
-   for(int i=0; i<3; i++)
-   {
-   	for(int j=0; j<3; j++)
-   	{
-   		cout << x(i,j) << " ";
-   	}
-
-   	cout << endl;
-   }
-
-
-   arma::mat y = randu<mat>(size(x));
-
-      cout << endl;
-   for(int i=0; i<3; i++)
-   {
-   	for(int j=0; j<3; j++)
-   	{
-   		cout << y(i,j) << " ";
-   	}
-
-   	cout << endl;
-   }
-
-   y.elem(find(y > 0.3)).fill(arma::randu());
+  for(int i=0; i<3; i++)
+   {   for(int j=0; j<3; j++)
+      {
+         for(int k=0; k<3; k++)
+         {
+            cout << x(k,j,i) << " ";
+         }
 
          cout << endl;
-   for(int i=0; i<3; i++)
-   {
-   	for(int j=0; j<3; j++)
-   	{
-   		cout << y(i,j) << " ";
-   	}
-
-   	cout << endl;
+      }
+     
+     cout << endl << endl;
    }
-	
+   
+   x.slice(0).fill(1);
+   x.slice(0)(8) = x.slice(1)(1);
 
+    for(int i=0; i<3; i++)
+   {   for(int j=0; j<3; j++)
+      {
+         for(int k=0; k<3; k++)
+         {
+            cout << x(k,j,i) << " ";
+         }
+
+         cout << endl;
+      }
+     
+     cout << endl << endl;
+   }
+
+   cout << x.n_elem << endl;
 	return 0;
 }
